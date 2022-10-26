69d988fcaf11b71ce7884b2d4e02fffbb1c37685
GaspardCulis <gaspard.culis@gmail.com>
2022-10-27 00:00:29 +0200

    Migrated and refactored login api

---
+++ src/pages/api/login.ts
@@ -0,0 +1,29 @@
+import { APIRoute } from "astro"
+import { isLoginValid, createAuthToken } from '../../model/database';
+import { addCookie } from "../../model/utils";
+
+export const post: APIRoute = async ({ request }) => {
+    const text = await request.text();
+	const username = new URLSearchParams(text).get("username");
+	const password = new URLSearchParams(text).get("password");
+
+    const headers = new Headers();
+	if (isLoginValid(username, password)) {
+		let { token, expires } = createAuthToken(username);
+		addCookie(headers, {
+			name: "token",
+			value: token,
+			expires: expires,
+			secure: true,
+			path: "/",
+		});
+		headers.append("Location", "/app/search");
+	} else {
+		headers.append("location", "../login?error=Invalid username or password");
+	}
+
+    return new Response("", {
+        status: 302,
+        headers,
+    });
+}
