1928da532d6a8aab45cd75fa4c90265a47b17756
Gaspard Culis <gaspard.culis@gmail.com>
2022-10-27 20:59:36 +0200

    Upgraded to node-yggtorrent 1.0.4

--- src/pages/api/search.ts
+++ src/pages/api/search.ts
@@ -1,10 +1,21 @@
 import { APIRoute } from "astro"
+import { YggTorrent, Categories, SubCategories } from "node-yggtorrent";
 
 export const post: APIRoute = async ({ request }) => {
-    const query = (await request.json());
-    console.log(query);
+    const query = (await request.json()).query;
+    
+    const ygg = new YggTorrent();
+    await ygg.initializeBrowser();
+    const results = await ygg.search({
+        name: query.search,
+        category: Categories.FILM_VIDEO,
+        subCategory: SubCategories.FILM_VIDEO[query.category],
+    });
+
+    console.log(results);
+
     return new Response(JSON.stringify({
-        message: "Your name was: " + "mamam"
+        results: results
       }), {
         status: 200
       })
