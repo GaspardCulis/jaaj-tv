192f100b2fb3897dd609c4bab28fb361f56bcb59
Gaspard Culis <gaspard.culis@gmail.com>
2022-11-01 20:34:23 +0100

    WAW actually good looking code

--- src/pages/api/search.ts
+++ src/pages/api/search.ts
@@ -1,57 +1,116 @@
 import { APIRoute } from "astro"
-import { YggTorrent, Categories, SubCategories, SortBy, SortOrder } from "node-yggtorrent";
+import { YggTorrent, Categories, SubCategories, SortBy, SortOrder, Torrent } from "node-yggtorrent";
 import axios from "axios";
-import cheerio from 'cheerio';
+import cheerio, { Cheerio, CheerioAPI, Element } from 'cheerio';
 import tnp from 'torrent-name-parser';
 import probe from 'probe-image-size';
 import { cacheResults, getCachedResults } from '../../model/database';
-import { GOOGLE_IMG_SCRAP , GOOGLE_QUERY } from 'google-img-scrap';
+import { FinalResult, GOOGLE_IMG_SCRAP } from 'google-img-scrap';
 
 const DEBUG = false;
 function debug(msg: string) {
   if (DEBUG) console.log(msg);
 }
 
-async function getImage(torrent_url: string) {
-  const response = await axios.get(torrent_url).catch((err) => {
-    console.log("Failed to get torrent page : ", torrent_url);
-  });
-  if (!response) return null;
-  const $ = cheerio.load(response.data);
-  const container = $(".default");
-  let image = "false";
-  debug("Finding image for " + torrent_url);
-  while (image=="false" || image.endsWith(".gif")) {
-    const img = container.find("img");
-    if (!img.length) {
-      debug("\tNo image found for : " + torrent_url); 
-      break;
-    }
-    image = img.attr("src") || "false";
-    debug("\tChecking image : " + image);
+abstract class ImageProvider {
+  abstract next(): Promise<string>;
 
-    if (image!== "false") {
-      let result = await probe(image).catch((err) => {debug("\tFailed to get image : " + image);});
+  abstract hasNext(): Promise<boolean>;
+}
 
-      if (result) {
-        const width = result?.width || 0;
-        const height = result?.height || 0;
-      
-        const ratio = width / height;
-        if (width < 100 || height < 100 || ratio >= 2) {
-          debug("\tImage too small or bad ratio, trying next one... : " + image);
-          image = "false";
-        }
-      } else {
-        image = "false";
-      }
-    }
-    
-    img.remove();
+class YggTorrentImageProvider extends ImageProvider {
+  private torrent_url: string;
+  private dom: CheerioAPI;
+  private index = 0;
+  private images: Element[];
+  
+  constructor(torrent_url: string) {
+    super();
+    this.torrent_url = torrent_url;
   }
 
-  debug("\tImage found : " + image);
-  return image==="false" ? null : image;
+  private async ensureDom(): Promise<boolean> {
+    if (!this.dom) {
+      const response = await axios.get(this.torrent_url).catch((err) => {
+        console.log("Failed to get torrent page : ", this.torrent_url);
+      });
+      if (response) {
+        this.dom = cheerio.load(response.data);
+        const container = this.dom(".default");
+        this.images = container.find("img").toArray();
+      };
+    }
+
+    return !!this.dom;
+  }
+
+  async next(): Promise<string> {
+    if (!await this.ensureDom()) return null;
+    
+    return this.images[this.index++].attribs.src;
+  }
+
+  async hasNext(): Promise<boolean> {
+    if (!await this.ensureDom()) return false;
+
+    return this.images && this.index < this.images.length;
+  }
+}
+
+class GoogleImageProvider extends ImageProvider {
+  private query: string;
+  private images: FinalResult[];
+  private index = 0;
+
+  constructor(query: string) {
+    super();
+    this.query = query;
+  }
+
+  async ensureImages(): Promise<boolean> {
+    if (!this.images) {
+      this.images = (await GOOGLE_IMG_SCRAP({search: this.query})).result;
+    }
+
+    return !!this.images;
+  }
+
+  async next(): Promise<string> {
+    if (!await this.ensureImages()) return null;
+    return this.images[this.index++].url;
+  }
+
+  async hasNext(): Promise<boolean> {
+    if (!await this.ensureImages()) return false;
+    return this.images && this.index < this.images.length;
+  }
+}
+
+async function isImageValid(image_url: string): Promise<boolean> {
+  if (image_url.endsWith(".gif")) return false;
+  let result = await probe(image_url).catch((err) => {debug("\tFailed to get image : " + image_url);});
+  if (!result) return false;
+  const ratio = result.width / result.height;
+  if(ratio < 0.5 || ratio > 0.9 || result.width < 300 || result.height < 300) return false;
+
+  return true;
+}
+
+async function getImage(torrent: Torrent, torrent_name: string) {
+  const image_providers = [
+    new YggTorrentImageProvider(torrent.url),
+    new GoogleImageProvider(torrent_name || torrent.name),
+  ];
+  for (const provider of image_providers) {
+    while (await provider.hasNext()) {
+      const image_url = await provider.next();
+      if (await isImageValid(image_url)) {
+        return image_url;
+      }
+    }
+  }
+
+  return null;
 }
 
 export const post: APIRoute = async ({ request }) => {
@@ -80,15 +139,11 @@ export const post: APIRoute = async ({ request }) => {
         let index = i;
         i++;
         const parsed_torrent = tnp(result.name);
-        let image = await getImage(result.url);
-        if (!image && (typeof parsed_torrent.title == 'string' && parsed_torrent.title.length>0)) {
-          image = (await GOOGLE_IMG_SCRAP({search: parsed_torrent.title})).result[0].url;
-        }
         out.push({
           id: result.id,
           ...parsed_torrent,
           baseName: result.name,
-          image:  image,
+          image: await getImage(result, parsed_torrent.title),
           index
         });
       }));
