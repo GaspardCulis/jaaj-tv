ae8ad644ee685f437b7328d432eb0fca778a209e
Gaspard Culis <gaspard.culis@gmail.com>
2022-11-11 21:24:05 +0100

    Added UserManager::getUserSync for faster load times

--- src/model/usermanager.ts
+++ src/model/usermanager.ts
@@ -5,16 +5,27 @@ export default class UserManager {
     private static users: Map<string, User> = new Map();
     
     static async getUser(login: string): Promise<User> {
+        const user = this.getUserSync(login);
+
+        await user.waitReady();
+
+        return user;
+    }
+
+    /**
+     * Use with caution, this method does not wait for user to be ready
+     */
+    static getUserSync(login: string): User {
         if (!this.users.has(login)) {
             this.users.set(login, new User(login));
             console.info(`${this.users.size} users connected`);
         }
 
         const user = this.users.get(login);
-        await user.waitReady();
 
         return user;
     }
+
 
     static async killUser(login: string) {
         if (this.users.has(login)) {
