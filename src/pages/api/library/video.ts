995504ea1eb2d9e55a038b42c2b1583a822fca5a
Gaspard Culis <gaspard.culis@gmail.com>
2022-11-16 03:50:42 +0100

    Yes a working video player ! (only mp4 bruh)

--- src/pages/api/library/video.ts
+++ src/pages/api/library/video.ts
@@ -1,12 +1,28 @@
 import { APIRoute } from "astro";
 import fs from "fs";
+import path from "path";
+import UserManager from "../../../model/usermanager";
+import { isAuthorized } from "../../../model/utils";
 
 export const get: APIRoute = async ({ request }) => {
+    const authorized = isAuthorized(request);
+    if (!authorized) return { status: 401, body: "Unauthorized" };
+
+    const movie_id = parseInt(new URL(request.url).searchParams.get("movie_id"));
+    if (!movie_id) return { status: 400, body: "Invalid request" };
+    const file_path = new URL(request.url).searchParams.get("file_path");
+    if (!file_path) return { status: 400, body: "Invalid request" };
+
+    const user = await UserManager.getUser(authorized as string);
+    const movie = user.getLibrary().getMovies().get(movie_id);
+    if (!movie) return { status: 417, body: "Invalid movie id" };
+    if (!fs.existsSync(path.join(user.getLibrary().getMovieDownloadPath(movie_id), file_path))) return { status: 417, body: "Invalid file path" };
+
     const range = request.headers.get("range");
     if (!range) {
         return {status: 400, body: "Requires Range header"};
     }
-    const videoPath = "C:\\Users\\gaspa\\Desktop\\test\\files\\out.webm";
+    const videoPath = path.join(user.getLibrary().getMovieDownloadPath(movie_id), file_path);
     const videoSize = fs.statSync(videoPath).size;
     const CHUNK_SIZE = 10 ** 6;
     const start = Number(range.replace(/\D/g, ""));
@@ -17,7 +33,7 @@ export const get: APIRoute = async ({ request }) => {
     headers.set("Content-Range", `bytes ${start}-${end}/${videoSize}`);
     headers.set("Accept-Ranges", "bytes");
     headers.set("Content-Length", contentLength.toString());
-    headers.set("Content-Type", "video/webm");
+    headers.set("Content-Type", "video/mp4");
     return new Response(videoStream as unknown as BodyInit, { headers, status: 206 });
 }
 
