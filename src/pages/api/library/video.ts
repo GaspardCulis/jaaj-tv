1b08d78f0c55aad34363dda763b6366eafdc35aa
Gaspard Culis <gaspard.culis@gmail.com>
2022-11-21 17:59:15 +0100

    Fixed fadeIn animation, why is video streaming this hard ?

--- src/pages/api/library/video.ts
+++ src/pages/api/library/video.ts
@@ -3,6 +3,7 @@ import fs from "fs";
 import path from "path";
 import UserManager from "../../../model/usermanager";
 import { isAuthorized } from "../../../model/utils";
+import Transcoder from 'stream-transcoder';
 
 export const get: APIRoute = async ({ request }) => {
     const authorized = isAuthorized(request);
@@ -18,21 +19,14 @@ export const get: APIRoute = async ({ request }) => {
     if (!movie) return { status: 417, body: "Invalid movie id" };
     if (!fs.existsSync(path.join(user.getLibrary().getMovieDownloadPath(movie_id), file_path))) return { status: 417, body: "Invalid file path" };
 
-    const range = request.headers.get("range");
-    if (!range) {
-        return {status: 400, body: "Requires Range header"};
-    }
     const videoPath = path.join(user.getLibrary().getMovieDownloadPath(movie_id), file_path);
     const videoSize = fs.statSync(videoPath).size;
-    const CHUNK_SIZE = 10 ** 6;
-    const start = Number(range.replace(/\D/g, ""));
-    const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
-    const contentLength = end - start + 1;
-    const videoStream = fs.createReadStream(videoPath, { start, end });
+    const videoStream = fs.createReadStream(videoPath);
+    //let body = new Transcoder(videoStream).videoCodec('libx264').format('mp4').stream();
+    
     const headers = new Headers();
-    headers.set("Content-Range", `bytes ${start}-${end}/${videoSize}`);
+    headers.set("Content-Range", `bytes */${videoSize}`);
     headers.set("Accept-Ranges", "bytes");
-    headers.set("Content-Length", contentLength.toString());
     headers.set("Content-Type", "video/mp4");
     return new Response(videoStream as unknown as BodyInit, { headers, status: 206 });
 }
