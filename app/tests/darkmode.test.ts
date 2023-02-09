16ae3fd690e8dd3d18b0dc5569b9a8a62857feb2
Gaspard Culis <gaspard.culis@gmail.com>
2023-02-09 17:06:14 +0100

    Remove darkmode test code

--- app/tests/darkmode.test.ts
+++ app/tests/darkmode.test.ts
@@ -7,12 +7,4 @@
  * @license: MIT License
  *
  */
-import { darkModeToggle, darkModeDetect } from "@components/common/darkmode/darkmode";
-
-test("darkModeToggle", async () => {
-	expect(darkModeToggle()).toBe(true || false);
-});
-
-test("darkModeDetect", async () => {
-	expect(darkModeDetect()).toBe(true || false);
-});
+export {};
