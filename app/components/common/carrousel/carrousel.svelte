df6220ad921ac0560ba542b750bbf4f9d6787baf
Gaspard Culis <gaspard.culis@gmail.com>
2023-02-11 18:46:26 +0100

    Window sucks my man

--- app/components/common/carrousel/carrousel.svelte
+++ app/components/common/carrousel/carrousel.svelte
@@ -1,13 +1,25 @@
 <script lang="ts">
+	import Movie from "./items/movie/movie.svelte";
 	import "./carrousel";
 
 	let selected = 0;
+	let items = [
+		{ src: "test/fight-club-backdrop-w780.jpg", alt: "Fight Club" },
+		{ src: "test/iron-man-backdrop-w780.jpg", alt: "Iron Man" },
+		{ src: "test/luca-backdrop-w780.jpg", alt: "Luca" },
+		{ src: "test/time-out-backdrop-w780.jpg", alt: "Time Out" },
+		{ src: "test/upgrade-backdrop-w780.jpg", alt: "Upgrade" },
+	];
 </script>
 
 <div class="carrousel vbox">
-	<div class="carrousel_container" />
+	<div class="carrousel_container">
+		{#each items as item, i}
+			<Movie src={item.src} alt={item.alt} hidden={selected !== i} />
+		{/each}
+	</div>
 	<div class="carrousel_controls hbox">
-		{#each [0, 1, 2, 3, 4] as _, i}
+		{#each items as _, i}
 			<span class="carrousel_control" class:selected={selected === i} on:click={() => (selected = i)} />
 		{/each}
 	</div>
---
+++ app/components/common/carrousel/items/movie/movie.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import "./movie";
+
+	export let src: string;
+	export let alt: string;
+	export let hidden: boolean;
+</script>
+
+<div class="movie_carrousel_item" class:hidden>
+	<img {src} {alt} />
+	<button> Watch now </button>
+</div>
+
+<style lang="scss">
+	@import "./movie.scss";
+</style>
